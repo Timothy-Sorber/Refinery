adeb50a8a8c380f8e31e960c323e22e29baab917
Timothy <sorbertimothy@gmail.com>
2023-01-29 21:09:55 -0500

    re-commit because some files dont want to commit automaticly

---
+++ src/main/java/org/refinery/util.java
@@ -0,0 +1,15 @@
+package org.refinery;
+
+public class util {
+    public static void wait(int ms)
+    {
+        try
+        {
+            Thread.sleep(ms);
+        }
+        catch(InterruptedException ex)
+        {
+            Thread.currentThread().interrupt();
+        }
+    }
+}
