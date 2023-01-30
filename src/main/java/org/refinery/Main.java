f08c0ac1a252ac72ef677b07c1eb698e27898862
Timothy <sorbertimothy@gmail.com>
2023-01-29 21:08:19 -0500

    added a window to show what the script is doing

--- src/main/java/org/refinery/Main.java
+++ src/main/java/org/refinery/Main.java
@@ -1,18 +1,36 @@
 package org.refinery;
 
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        util u = new util();
+        JFrame w = new JFrame();
+        w.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        w.setSize(1000,1000);
+        w.setVisible(true);
+        JLabel l = new JLabel();
+        l.setHorizontalAlignment(JLabel.CENTER);
+        l.setText("Hello world! This program will run a basic 3X+1 script in 3 seconds... [3]");
+        w.getContentPane().add(l);
+        u.wait(1000);
+        l.setText("Hello world! This program will run a basic 3X+1 script in 3 seconds... [2]");
+        u.wait(1000);
+        l.setText("Hello world! This program will run a basic 3X+1 script in 3 seconds... [1]");
+        u.wait(1000);
+        l.setText("Hello world! This program will run a basic 3X+1 script in 3 seconds... [0]");
+        System.out.println("Running script");
         long process = 1;
         long n = 1;
         int i = 0;
-        int test = 4000;
+        long test = 4000000;
         long h = 1;
         Boolean c = true;
         int pi = 0;
         while (c) {
             if (process <= 4) {
-                System.out.println("Seed " + Long.toString(n) + " found true after " + Integer.toString(pi) + " iterations");
+                System.out.println("Seed " + n + " found true after " + pi + " iterations");
                 n++;
                 process = n;
                 pi = 0;
@@ -28,9 +46,11 @@ public class Main {
             if (process > h){
                 h = process;
             }
+            l.setText("Current int: " + process + " | Highest integer reached: " + h + " | Seed being tested: " + n);
             if (i >= test){
                 c = false;
-                System.out.println("Reached max tests, highest integer reached: " + Long.toString(h));
+                System.out.println("Reached max tests, highest integer reached: " + h);
+                l.setText("Max Tests reached, please set the int 'test' to a higher value to test further || Highest value reached is " + h);
             }
         }
     }
